0f50b616da1c80bc12e64127c7ed0194ee5ad79c
vroby65 <vroby.mail@gmail.com>
2015-02-12 09:36:44 +0100

    add userlist

--- client.php
+++ client.php
@@ -10,14 +10,16 @@
 <form name='chat' action="<?=$server?>?alt=html" method='post' enctype='multipart/form-data' target='display' onsubmit="message.value=messageEntry.value;messageEntry.value='';" >
 room <input type='text' name='room' id='room' value="<?=$room ?>">
 <input type="button" value='cancella' onclick="location='<?=$server?>?delete=yes&room='+document.chat.room.value" target='display'> <br> 
-<iframe name='display' id='display' onload='display.scrollTo(0,10000);'></iframe><br>
+<iframe name='display' id='display' onload='display.scrollTo(0,10000);'></iframe>
+<iframe name='listusers' id='listusers' onload='display.scrollTo(0,10000);'></iframe>
+<br>
 user <input type='text' name='user' value="<?=$user ?>" ><br>
 <input type='hidden' name='message' value="<?=$message ?>" >
 message <input type='text' name='messageEntry'><button >invia</button><br>
 </form>
 </body>
 <script>
-	setInterval(function(){display.location="<?=$server?>?alt=html&room="+document.chat.room.value;},5000);
+	setInterval(function(){display.location="<?=$server?>?scope=html&room="+document.chat.room.value;listusers.location="<?=$server?>?scope=listhtml&room="+document.chat.room.value;},5000);
 	if (document.chat.room.value=='')document.chat.room.value=prompt("please enter room name");
 	if (document.chat.user.value=='')document.chat.user.value=prompt("please enter username");
 	document.chat.submit();
--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-	$alt=$_GET['alt'];
+	$scope=$_GET['scope'];
 	$room=$_GET['room'];if($room=='')$room=$_POST['room'];
 	$user=$_GET['user'];if($user=='')$user=$_POST['user'];
 	$message=$_GET['message'];if($message=='')$message=$_POST['message'];
@@ -7,15 +7,41 @@
 	$delete=$_GET['delete'];
 	if($delete!="" && $room!=""){
 		unlink ("rooms/$room");
+		unlink ("rooms/$room".".list");		
 		echo "<script>location='./';</script>";
 	}
 	
 	if($room!="" && $user!='' && $message!=''){
+		$lu=@file("rooms/$room".".list");
+		$listuser=join($lu);
+		for($i=0;$i<count($lu);$i++){
+			if(date($lu[$i])< date('d-m-Y H:i:s',strtotime("$data - 1 hour"))||strpos($listuser,$user)!=false){
+				unset($lu[$i]);
+				$listuser=join($lu);
+				unlink ("rooms/$room".".list");		
+				$f=fopen("rooms/$room".".list","w");
+				fwrite($f,join($lu));
+				fclose($f);					
+			}
+		}
+		if(strpos($listuser,$user)==false){
+			$f=fopen("rooms/$room".".list","a");
+				fwrite($f,date("Y-m-d H:i:s")."-".$user."\n");
+			fclose($f);			
+		}
 		$f=fopen("rooms/$room","a");
 			fwrite($f,$user.":".$message."\n");
 		fclose($f);
 	}
-	switch ($alt){
+	switch ($scope){
+	case "listhtml":
+		echo str_replace("\n","<br>",join(@file("rooms/$room".".list")));
+		break;
+	case "list":
+		echo join(@file("rooms/$room".".list"));
+		break;
+	
+	
 	case "html":
 		echo str_replace("\n","<br>",join(@file("rooms/$room")));
 		break;	
